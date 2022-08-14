44fe663ef995956de44bad9f583a97fd62862e59
Fernando <fernando.bbosco@gmail.com>
2022-08-13 21:24:58 -0300

    initial:template

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { Center } from "@chakra-ui/react";
+import AppButton from "./components/Button";
+
+export default function App() {
+  return (
+    <Center h="100vh">
+      <AppButton>Do Something!</AppButton>
+    </Center>
+  );
+}
